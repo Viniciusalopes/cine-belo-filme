f5dc97fe546bdbd9c24c00d01a02c02bac56541c
viniciusalopes <suporte@viniciusalopes.com.br>
2019-12-03 20:58:06 -0300

    Modelo de objeto da classe Usuario

--- CineBeloFilme/src/model/Usuario.java
+++ CineBeloFilme/src/model/Usuario.java
@@ -1,37 +1,49 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * ---------------------------------------------------------------------------------------
+ * Licença   : MIT - Copyright 2019 Viniciusalopes (Vovolinux) <suporte@vovolinux.com.br>
+ *             <https://opensource.org/licenses/MIT>
+ * ---------------------------------------------------------------------------------------
+ * Criado em : novembro de 2019
+ * ---------------------------------------------------------------------------------------
+ * Projeto   : Projeto Integrador - Cine ABC
+ * ---------------------------------------------------------------------------------------
+ * Alunos    : Gustavo Henrique Ribeiro Martins
+ *             Olair Soares de Almeida
+ *             Vinicius Araujo Lopes
+ * ---------------------------------------------------------------------------------------
+ * Finalidade: Modelo de objeto da classe Usuario
+ * ---------------------------------------------------------------------------------------
  */
 package model;
 
-/**
- *
- * @author vovostudio
- */
 public class Usuario {
+
+    // Atributos
     private String tipo;
     private String cpf;
 
+    // Construtor
     public Usuario() {
         this.tipo = "admin";
         this.cpf = "";
     }
 
+    // Get
     public String getTipo() {
         return tipo;
-    }
-
-    public void setTipo(String tipo) {
-        this.tipo = tipo;
     }
 
     public String getCpf() {
         return cpf;
     }
 
+    // Set
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
     public void setCpf(String cpf) {
         this.cpf = cpf;
     }
-    
+
 }
