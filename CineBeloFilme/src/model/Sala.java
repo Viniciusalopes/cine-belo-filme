14447723e5ac4611a67ccda2dc7105e3e07dab7a
viniciusalopes <suporte@viniciusalopes.com.br>
2019-12-03 20:55:54 -0300

    Modelo de objeto da classe Sala

--- CineBeloFilme/src/model/Sala.java
+++ CineBeloFilme/src/model/Sala.java
@@ -1,28 +1,38 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * ---------------------------------------------------------------------------------------
+ * Licença   : MIT - Copyright 2019 Viniciusalopes (Vovolinux) <suporte@vovolinux.com.br>
+ *             <https://opensource.org/licenses/MIT>
+ * ---------------------------------------------------------------------------------------
+ * Criado em : novembro de 2019
+ * ---------------------------------------------------------------------------------------
+ * Projeto   : Projeto Integrador - Cine ABC
+ * ---------------------------------------------------------------------------------------
+ * Alunos    : Gustavo Henrique Ribeiro Martins
+ *             Olair Soares de Almeida
+ *             Vinicius Araujo Lopes
+ * ---------------------------------------------------------------------------------------
+ * Finalidade: Modelo de objeto da classe Sala
+ * ---------------------------------------------------------------------------------------
  */
 package model;
 
 import java.util.List;
 
-/**
- *
- * @author vovostudio
- */
 public class Sala {
 
+    // Atributos
     private int id;
     private String nome;
     private List<Sessao> sessoes;
 
+    // Construtor
     public Sala(int id, String nome, List<Sessao> sessoes) {
         this.id = id;
         this.nome = nome;
         this.sessoes = sessoes;
     }
 
+    // Métodos
     public int getId() {
         return id;
     }
