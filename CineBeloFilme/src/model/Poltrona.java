1a91d9b6381cdc9164449e9d2de0db8aa2e2f86d
viniciusalopes <suporte@viniciusalopes.com.br>
2019-12-03 20:55:15 -0300

    Modelo de objeto da classe Poltrona

--- CineBeloFilme/src/model/Poltrona.java
+++ CineBeloFilme/src/model/Poltrona.java
@@ -1,27 +1,37 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * ---------------------------------------------------------------------------------------
+ * Licença   : MIT - Copyright 2019 Viniciusalopes (Vovolinux) <suporte@vovolinux.com.br>
+ *             <https://opensource.org/licenses/MIT>
+ * ---------------------------------------------------------------------------------------
+ * Criado em : novembro de 2019
+ * ---------------------------------------------------------------------------------------
+ * Projeto   : Projeto Integrador - Cine ABC
+ * ---------------------------------------------------------------------------------------
+ * Alunos    : Gustavo Henrique Ribeiro Martins
+ *             Olair Soares de Almeida
+ *             Vinicius Araujo Lopes
+ * ---------------------------------------------------------------------------------------
+ * Finalidade: Modelo de objeto da classe Poltrona
+ * ---------------------------------------------------------------------------------------
  */
 package model;
 
-/**
- *
- * @author vovostudio
- */
 public class Poltrona {
 
+    // Atributos
     private int id;
     private char fileira;
     private int numero;
 
+    // Construtor
     public Poltrona(int id, char fileira, int numero) {
         this.id = id;
         this.fileira = fileira;
         this.numero = numero;
-        
+
     }
 
+    // Get
     public int getId() {
         return id;
     }
