c20d0de3f8d285b0f23560215bf8609763fbdaea
viniciusalopes <suporte@viniciusalopes.com.br>
2019-12-03 20:54:06 -0300

    Modelo de objeto da classe Filme

--- CineBeloFilme/src/model/Filme.java
+++ CineBeloFilme/src/model/Filme.java
@@ -1,24 +1,34 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * ---------------------------------------------------------------------------------------
+ * Licença   : MIT - Copyright 2019 Viniciusalopes (Vovolinux) <suporte@vovolinux.com.br>
+ *             <https://opensource.org/licenses/MIT>
+ * ---------------------------------------------------------------------------------------
+ * Criado em : novembro de 2019
+ * ---------------------------------------------------------------------------------------
+ * Projeto   : Projeto Integrador - Cine ABC
+ * ---------------------------------------------------------------------------------------
+ * Alunos    : Gustavo Henrique Ribeiro Martins
+ *             Olair Soares de Almeida
+ *             Vinicius Araujo Lopes
+ * ---------------------------------------------------------------------------------------
+ * Finalidade: Modelo de objeto da classe Filme
+ * ---------------------------------------------------------------------------------------
  */
 package model;
 
-/**
- *
- * @author vovostudio
- */
 public class Filme {
 
+    // Atributos
     private int id;
     private String titulo;
 
+    // Construtor
     public Filme(int id, String titulo) {
         this.id = id;
         this.titulo = titulo;
     }
 
+    // Get
     public int getId() {
         return id;
     }
